0fa98fb5d2c7daa4480203745f9902c21b9fe429
spacebuckett <shehxadv2.0@gmail.com>
2025-08-16 13:46:35 +0500

    now printing an entire line. To check for a new line we used bytes.IndexByte(data, n)

--- httpfromtcp/main.go
+++ httpfromtcp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -15,6 +16,7 @@ func main() {
 
 	defer f.Close()
 
+	str := ""
 	for {
 		data := make([]byte, 8)
 		n, err := f.Read(data)
@@ -24,6 +26,17 @@ func main() {
 			break
 		}
 
-		fmt.Printf("read %s\n", string(data[:n]))
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i+1 : n]
+			fmt.Printf("read %s\n", str)
+			str = ""
+		}
+		str += string(data)
+	}
+
+	if len(str) != 0 {
+		fmt.Printf("read %s\n", str)
 	}
 }
